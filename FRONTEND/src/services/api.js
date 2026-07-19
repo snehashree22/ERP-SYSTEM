fc851a37bc50b14cd871235d644de4da991d1c44
snehashree22 <snehashree.22bcb7068@vitapstudent.ac.in>
2026-07-19 17:15:22 +0530

    Switch to Neon database and update API config

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://retailflow-api.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
 });
 
 api.interceptors.request.use((config) => {
